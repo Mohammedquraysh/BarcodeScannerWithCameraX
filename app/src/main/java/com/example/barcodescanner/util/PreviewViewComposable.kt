6113d6e20eab2cbd22634467347b76dbc9c42c1f
Mohammedquraysh <mohammedquraysh4@gmail.com>
2023-11-10 12:46:04 +0100

    added barcode scanner

---
+++ app/src/main/java/com/example/barcodescanner/util/PreviewViewComposable.kt
@@ -0,0 +1,70 @@
+package com.example.barcodescanner.util
+
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import com.example.barcodescanner.analyser.BarcodeAnalyser
+import java.util.concurrent.Executors
+
+@androidx.camera.core.ExperimentalGetImage
+@Composable
+fun PreviewViewComposable() {
+    AndroidView({ context ->
+        val cameraExecutor = Executors.newSingleThreadExecutor()
+        val previewView = PreviewView(context).also {
+            it.scaleType = PreviewView.ScaleType.FILL_CENTER
+        }
+//        CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK)
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+
+            val imageCapture = ImageCapture.Builder().build()
+
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, BarcodeAnalyser{
+                        Toast.makeText(context, "Barcode found", Toast.LENGTH_SHORT).show()
+                    })
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    context as ComponentActivity, cameraSelector, preview, imageCapture, imageAnalyzer)
+
+            } catch(exc: Exception) {
+                Log.e("MQ", "Use case binding failed", exc)
+            }
+        }, ContextCompat.getMainExecutor(context))
+        previewView
+    },
+        modifier = Modifier
+            .size(width = 250.dp, height = 250.dp))
+}
+
+
